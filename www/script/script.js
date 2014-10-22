e002a6871dde73d9fdc39b4795f4570d115dd10b
kavyasukumar <kavya.sukumar@hotmail.com>
2014-10-22 11:06:17 -0400

    Prevented reset on resize

--- www/script/script.js
+++ www/script/script.js
@@ -1,33 +1,40 @@
 var $data;
+var currentSelection = {'type':'linear', 'prop':'no'};
 $(document).ready(function () {
-    d3.json("./data/data.json", function (error, d) {
+    d3.json("http://apps.mypalmbeachpost.com/interactives/goodmanjurors/data/data.json", function (error, d) {
         $data = d;
         drawViz();
     });
-    $(window).resize(drawViz);
+    $(window).resize(function(){
+        drawViz();
+    });
 });
 
-function drawViz(){
+function drawViz(){    
 	var $windowH = $(window).height();
-    var $windowW = $('body').width();
+    var $windowW = $('#fakebody').width();
 
     var $radius = 30,
         $vpadding =5;
 
+    var flip = false;
+
     var margin = { top: 80, right: 20, bottom: 30, left: 50 };
-    if($windowW < 481)
+    if($windowW < 680)
     {    
-        margin = { top: 40, right: 0, bottom: 0, left: 20 };
+        flip = true;
+        margin = { top: 40, right: 0, bottom: 0, left: 70 };
     }
     
-    var width =  $windowW,// - margin.left - margin.right-10,
-    height = Math.max($windowH,500) - margin.top - margin.bottom;
+    var width =  $windowW - margin.left - margin.right-10,
+    height = Math.max($windowH,600) - margin.top - margin.bottom;
     //height = $('#sortselectors').height() - margin.top - margin.bottom;
 
-    $radius= Math.min(width/18,height/18); //todo: update this to max in a group 
+    $radius= Math.min(width/14,height/24); 
     // add a svg canvas
 
     d3.select("#viz").select("svg").remove();
+    //updateSelectionStyle($('#no'));
 
     var svg = d3.select("#viz").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -50,6 +57,13 @@ function drawViz(){
         .scale(ordinalScale)
         .orient("top");
 
+    if(flip){
+        linearAxis.orient("left");
+        ordinalAxis.orient("left");
+        ordinalScale.rangePoints([0,height*2/3-2*$radius],.1);
+        linearScale.range([0,height-2*$radius]);
+    }
+
     var addAxis = function(axis){ 
         removeAxis();
         
@@ -64,8 +78,19 @@ function drawViz(){
     // calculate transform for ordinal axis
     var getTransform = function(propValue){
             countArr[propValue] = countArr[propValue]==undefined ? 0 : countArr[propValue]+1;
-            var y = $radius + 2*countArr[propValue]*($radius+$vpadding);
-            return "translate("+ordinalScale(propValue)+","+y+")";
+            var x = ordinalScale(propValue);            
+            var currentindex = countArr[propValue]            
+            var y = $radius + 2*currentindex*($radius+$vpadding);
+
+            if(currentindex>4){
+                var currentindex = countArr[propValue]%5;
+                x = x + 2*$radius;
+                y = $radius + 2*(currentindex)*($radius+$vpadding);
+            }
+            if(flip){
+                return "translate("+y+","+x+")";
+            }
+            return "translate("+x+","+y+")";
         }
 
     var delayFx = function(d,i){
@@ -84,11 +109,21 @@ function drawViz(){
     var jurors = svg.selectAll('.juror')
     	.data($data)
     .enter().append('g')        
-        .attr('class','juror')
-        .attr('transform', function(d){return "translate("+linearScale(d.no)+",30)";});
+        .attr('class','juror');
+
+    if(currentSelection.type == 'linear'){
+        linearTransformer(currentSelection.prop);
+    }
+    else{
+        ordinalTransformer(currentSelection.prop);
+    }
+
+
 
     jurors.append('image')
-        .attr("xlink:href",function(d){ return"./icons/"+ (d.type=="Main"?"":"gray")+d.gender +'.svg';})
+        .attr("xlink:href",function(d){ 
+            return"http://apps.mypalmbeachpost.com/interactives/goodmanjurors/icons/"+ (d.type=="Main"?"":"gray")+d.gender +'.svg';
+        })
         .attr('x',-$radius)
         .attr('y',-$radius)
         .attr("width", 2*$radius)
@@ -100,62 +135,80 @@ function drawViz(){
         .attr('fill','white')
         .attr('font-size',$radius/2)
         .attr('stroke','white')
-    	.text(function(d){return d.originalno;});
+    	.text(function(d){return d.no;});
 
 
-    // sort behavior
-    d3.selectAll('.selector.linear')
-    	.on('click', function(){
-
-           updateSelectionStyle(d3.event.target);
-
-            var prop = d3.event.target.id; //get the property name to sort by
-
-            //set up linear axis
+    function linearTransformer(prop){
+        //set up linear axis
             linearScale.domain([1,d3.max($data, function (d) { return d[prop]; })]);
 
             // animate transition 
-    		svg.selectAll('.juror')
-    		   .transition()
+            svg.selectAll('.juror')
+               .transition()
                .duration(800)
                .delay(delayFx)
-               .attr('transform', function(d,i){return "translate("+linearScale(d[prop])+",30)";});
+               .attr('transform', function(d,i){
+                if(flip){
+                    return "translate(30,"+linearScale(d[prop])+")"
+                }
+                return "translate("+linearScale(d[prop])+",30)";
+            });
+
+            addAxis(linearAxis);
 
 
-            addAxis(linearAxis);           
+            //set currents election in case of resize            
+            currentSelection.type ='linear';
+            currentSelection.prop = prop;   
+
 
             //special casing the index    
             if(prop =="no"){
                 removeAxis();
         }
+    }
+    // sort behavior
+    d3.selectAll('.selector.linear')
+    	.on('click', function(){
+           updateSelectionStyle(d3.event.target);           
+            linearTransformer(d3.event.target.id);            
     });
+
+    function ordinalTransformer(prop){
+        //set up axis
+            ordinalScale.domain($data.map(function (d){ return d[prop];}));
+            addAxis(ordinalAxis);
+
+            //now calculate transforms and animate transition
+            countArr = {};
+            svg.selectAll('.juror')         
+              .transition()
+               .duration(800)
+               .delay(delayFx)
+              .attr('transform', function(d){ return getTransform(d[prop]);});
+
+
+            //set currents election in case of resize            
+            currentSelection.type ='ordinal';
+            currentSelection.prop = prop; 
+    }
 
     // group behavior
     d3.selectAll('.selector.ordinal')
     	.on('click', function(){
             updateSelectionStyle(d3.event.target);
 
-            var prop = d3.event.target.id; //get the property name to sort by
+            ordinalTransformer(d3.event.target.id); //get the property name to sort by
 
-            //set up axis
-            ordinalScale.domain($data.map(function (d){ return d[prop];}));
-            addAxis(ordinalAxis);
-
-            //now calculate transforms and animate transition
-            countArr = {};
-    		svg.selectAll('.juror')    		
-    		  .transition()
-               .duration(800)
-               .delay(delayFx)
-              .attr('transform', function(d){ return getTransform(d[prop]);});
-    	});
+            
+        });
 
     // show modal on clicking juror
     d3.selectAll('.juror')
     .on('click', function(){            
-        d3.select('.modal #content').remove();
+        d3.select('.mymodal #content').remove();
         
-        var contentDiv = d3.select('.modal')
+        var contentDiv = d3.select('.mymodal')
         .append('div')
         .attr('id','content');
 
@@ -163,54 +216,30 @@ function drawViz(){
             .classed('profile', true)
             .attr('src',d3.event.target.href.baseVal);
 
-        var getPronoun = function (gender){
-            return gender=="Female"?"She":(gender=="Male"?"He":null);
-        }
-
         contentDiv.append('div')
             .classed('props',true)
             .html(function(d){
                 var propset = d3.event.target.__data__;
 
-                var html ="<span id=\"modaljurorno\"> <b>Juror #"+propset.originalno+"</b></span>"
+                var html ="<span id=\"modaljurorno\"> <b>Juror #"+propset.no+"</b></span>"
                     +"<span id=\"modalcity\"><b> City</b>: "+propset.city+"</span>"
                     +"<span id=\"modalgender\"><b> Gender</b>: "+propset.gender+"</span>"
-                    +"<span id=\"modalrace\"><b> Race</b>: "+propset.race+"</span>"
+                    +"<span id=\"modalrace\"><b> Race</b>: "+propset.race+"</span>"                    
+                    +"<span id=\"modaloccupation\"><b> Occupation</b>: "+propset.occupation+"</span>"
                     +"<span id=\"modalmarital\"><b> Marital status</b>: "+propset.marital+"</span>"
-                    +"<span id=\"modaloccupation\"><b> Occupation</b>: "+propset.occupation+"</span><br/>"
+                    +"<span id=\"modalchildren\"><b> No. of children</b>: "+propset.children+"</span><br/>"
                     +"<span id=\"modalfunfact\">"+propset.funfact+"</span><br/>";
-
-                   /* +"<span>" + getPronoun(propset.gender) + " has "+(propset.priorjury == "No"?"never":"")+" served on a jury before this trial.</span>"
-                    +"<span>" + getPronoun(propset.gender) + " has "+(propset.lawenforcement == "No"?"never":"")+" worked in law enforcement.</span>"
-                    +"<span>" + getPronoun(propset.gender) + " has "+(propset.witness == "No"?"never":"")+" been a witness to a crime.</span>"
-                    +"<span>" + getPronoun(propset.gender) + " has "+(propset.victim == "No"?"never":"")+" been a victim of a crime.</span>"
-                    +"<span>" + getPronoun(propset.gender) + " has "+(propset.arrested == "No"?"never":"")+" been arrested.</span>"
-                    +"<span>" + getPronoun(propset.gender) + " has "+(propset.lawsuit == "No"?"never":"")+" been involved in a lawsuit.</span>"
-                    +"<span>" + getPronoun(propset.gender) + " has "+(propset.claims == "No"?"never":"")+" filed claims for personal injury.</span>"*/
                     return html;
             });
 
-
-
-       /* contentDiv.selectAll('div')
-            .data(d3.entries(d3.event.target.__data__))
-            .enter().append('div')
-            .html(function(d){
-                if(propmap[d.key] != undefined)
-                {
-                    var key = propmap[d.key];
-                }
-                return '<span class="key">'+d.key+'</span>:&nbsp;' +'<span>'+d.value+'<\span>';
-            });*/
-
-        d3.select('.modal')
+        d3.select('.mymodal')
         .classed('show',true);
     })
 
     //close modal window
     d3.select('#closeBtn')
     .on('click', function(){
-        d3.select('.modal')
+        d3.select('.mymodal')
         .classed('show',false);
     })
 }
